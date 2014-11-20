6eead72bbbaf033aafd03137965594a9f5434bd9
thienchuong9x <thienchuong.la91@gmail.com>
2014-11-20 17:26:40 +0700

    edit ten control

---
+++ TDNK/SanPhamChung.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SanPhamChung : System.Web.UI.UserControl
+{
+    public string MaLoaiSP { get; set; }
+    public string MaSP { get; set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
